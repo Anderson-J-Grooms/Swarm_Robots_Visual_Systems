ad9abbdc0d1a4b48ce5989a1cc7053dbbaf69d36
Isaac Nelson <idn0971@gmail.com>
2023-02-17 11:53:55 -0500

    Added updated camera code

---
+++ camera/CMakeFiles/square_detect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/square_detect.dir/square_detect.cpp.o"
+  "square_detect"
+  "square_detect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/square_detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ camera/CMakeFiles/squares_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/squares_test.dir/squares_test.cpp.o"
+  "squares_test"
+  "squares_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/squares_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
