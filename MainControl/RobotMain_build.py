e4c55bcd95eedd4903539fbab6aa80f83d01ff32
Anderson Grooms <groomsaj@miamioh.edu>
2023-03-13 18:22:16 -0400

    Updated main control files

--- MainControl/RobotMain_build.py
+++ MainControl/RobotMain_build.py
@@ -1,20 +1,22 @@
-from cffi import FFI
-ffibuilder = FFI()
+def compileMain():
+     ffibuilder = FFI()
 
-# cdef() expects a single string declaring the C types, functions and
-# globals needed to use the shared object. It must be in valid C syntax.
-ffibuilder.cdef("""
-    int get_state(int pin);
-""")
-# set_source() gives the name of the python extension module to
-# produce, and some C source code as a string.  This C code needs
-# to make the declarated functions, types and globals available,
-# so it is often just the "#include".
-ffibuilder.set_source("_encodercontrol_cffi",
-"""
-     #include "encodercontrol.h"   // the C header of the library
-""",
-     libraries=['encodercontrol'])   # library name, for the linker
+     # cdef() expects a single string declaring the C types, functions and
+     # globals needed to use the shared object. It must be in valid C syntax.
+     ffibuilder.cdef("""
+     void init_control();
+     void clean_up_control();
+     int get_state(int pin);
+     """)
+     # set_source() gives the name of the python extension module to
+     # produce, and some C source code as a string.  This C code needs
+     # to make the declarated functions, types and globals available,
+     # so it is often just the "#include".
+     ffibuilder.set_source("_encodercontrol_cffi",
+     """
+          #include "encodercontrol_export.h"   // the C header of the library
+     """,
+          libraries=['encodercontrol'])   # library name, for the linker
 
-if __name__ == "__main__":
-    ffibuilder.compile(verbose=True)
+     if __name__ == "__main__":
+          ffibuilder.compile(verbose=True)
