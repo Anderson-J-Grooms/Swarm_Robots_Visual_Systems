3670622c48a3efcc62c6c1e2a8f10c0c78812fcd
Kyle Jacob Newman <kylenewman@Kyles-MacBook-Pro-3.local>
2023-04-29 12:01:21 -0400

    Updated function

--- MainControl/angleToIntercept.py
+++ MainControl/angleToIntercept.py
@@ -1,12 +1,19 @@
 import numpy as np
 
 # Uses the data from the camera to determine the angle the robot needs to turn to intercept the other robot.
-def angleToTurn(distance, theta0, theta1, runnerSpeed, chaserSpeed, sideOfRobot):
+def angleToIntercept(distance, theta0, theta1, runnerSpeed, chaserSpeed):
     # Check with lam on the logic
-    if (theta0 < 0): # Robot is on the left
-        theta = 180 + theta0 - theta1
+    if (theta0 > 0): # Robot is on the left
+        if(theta1 > 90 or theta1 > 270): # Robot is going left
+            theta = 180 + theta0 - theta1
+        else:
+            theta = theta1 - theta0
     else: # Robot is on the right
-        theta = 180 + theta0 + theta1
+        if(theta1 > 90 or theta1 > 270): # Robot is going left
+            theta = 180 + theta0 + theta1
+        else:
+            theta = theta0 - theta1
+
     b = 2*distance*runnerSpeed*np.cos(theta)
     a = np.power(chaserSpeed, 2)-np.power(runnerSpeed, 2)
     c = np.power(distance,2)
