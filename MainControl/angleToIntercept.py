f544ca3a833113d4ca47b6fd392e42d36c0eae38
Kyle Jacob Newman <kylenewman@Kyles-MacBook-Pro-3.local>
2023-04-29 12:54:16 -0400

    Fixed angle function, gives output in degrees Also did a quick test code

--- MainControl/angleToIntercept.py
+++ MainControl/angleToIntercept.py
@@ -13,14 +13,19 @@ def angleToIntercept(distance, theta0, theta1, runnerSpeed, chaserSpeed):
             theta = 180 + theta0 + theta1
         else:
             theta = theta0 - theta1
-
+    theta = np.deg2rad(theta)
+    theta0 = np.deg2rad(theta0)
+    theta1 = np.deg2rad(theta1)
     b = 2*distance*runnerSpeed*np.cos(theta)
     a = np.power(chaserSpeed, 2)-np.power(runnerSpeed, 2)
     c = np.power(distance,2)
     time = (-b+np.sqrt(np.power(b,2)+4*a*c))/(2*a)
     # D = (cos(theta0)*d, sin(theta0)*d)
-    Dy = np.sin(theta0)*d
+    Dy = np.sin(theta0)*distance
     # VR = (cos(theta1)*runnerSpeed, sin(theta1)*runnerSpeed)
     VRy = np.sin(theta1)*runnerSpeed
     y = (Dy+VRy*time)/time
-    return np.arccos(y/chaserSpeed)
+    print("{} {}".format(y,chaserSpeed))
+    angle = np.arccos(y/chaserSpeed)
+    angle = np.rad2deg(angle)
+    return angle
