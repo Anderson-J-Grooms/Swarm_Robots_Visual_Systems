87ab5e5b76074aab4d03abdda6c9f498c2cfc0d6
jackdelv <jackdelv39@gmail.com>
2023-04-29 10:40:21 -0400

    Tidy up a bit…
    Completely reorganize repository. The RGB, camera, and MainControl code is now under the src directory. The Report and images have their own root directory now. The binaries, log files, and configuration files.
    
    Signed-off-by: jackdelv <jackdelv39@gmail.com>

--- RGB/RGB/mainTesting.py
+++
@@ -1,6 +0,0 @@
-from command import*
-
-print("command is: ", command(0,0,0))
-print("command is: ", command(5472,0,0))
-print("command is: ", command(0,5472,0))
-print("command is: ", command(0,0,5472))
